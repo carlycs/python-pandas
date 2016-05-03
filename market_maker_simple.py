a806d66d109f3c0bab319dbbafc37a1ad27c88e7
hpcorner <hp@lotustech.io>
2016-05-03 14:14:16 -0400

    project close- bad code

--- market_maker_simple.py
+++ market_maker_simple.py
@@ -4,21 +4,26 @@ import math
 import scipy.linalg
 import pandas as pd
 
+
 S0 = 100.00 # for midprice
 T  = 30 # terminal date
 std = 0.01
 alpha = 0.0001
 #alpha=0
-N = 30 # inventory
+N = 30 # inventory ??
 drifts = [-0.2,0,0.2]
 q_up = 3 # upper bound
 q_low = -3
-q = 10        
+q = 1        
 kappa = 100
 #phi = 0
 phi = 0.00001
 lambda_pos = 1
 lambda_neg = 1
+
+
+#hack
+delta_t=1
 
 def makeWienerIncrements(delta_t,N):
     return float(math.sqrt(delta_t))*numpy.random.randn(N)
@@ -47,28 +52,35 @@ def buy_optimal_depth(t,q,kappa):
 def h(t,q):
     return (math.log(w(t,q))*1.0/kappa)
 
+#this part is not coded well and the equations are misplaced. The upper
+#and lower bounds are also manipulated everywhere making it harded to debug. 
+#this code is basically useless.
 def w(t,q):
-	index = range(q_low,q_up+1)
-	df = pd.DataFrame(index=index, columns=index)
-	df = df.fillna(0)
-	for i in range(q_low,q_up+1):
-		if i == q:
-			df.ix[i,q] = -1*phi*q*q
-		elif i == q-1:
-		   	df.ix[i,q] = lambda_pos/math.e
-		elif i == q+1:
-		   	df.ix[i,q] = lambda_neg/math.e
-		else:
-		    	0
-	temp = df*(T-t)
-	z = pd.DataFrame(index=['0'], columns=index)
-	z = z.fillna(0)
-	for i in range(q_low,q_up+1):
-		z.ix[0,i] = math.exp(-1*alpha*kappa*i*i)
-	tempnp = temp.as_matrix()
-	znp = z.as_matrix().ravel()
-	temp2 = numpy.dot(scipy.linalg.expm(tempnp),znp)
-	return temp2.ix[q]
+    index = range(-3,3)#range(q_low,q_up+1)
+    df = pd.DataFrame(index=index, columns=index)
+    df = df.fillna(0)
+    for i in range(-3,3): #range(q_low,q_up+1):
+        for q in range(-3,3): #range(q_low,q_up+1):
+             if i == q:
+                             df.loc[i,q] = -1*q*q  #phi scaled 
+             elif i == q-1:
+                 df.loc[i,q] = lambda_pos/math.e
+             elif i == q+1:
+                 df.loc[i,q] = lambda_neg/math.e
+            #                 else:
+            #                     0
+                 return df
+                 
+                 
+#	temp = df*(T-t)
+#	z = pd.DataFrame(index=['0'], columns=index)
+#	z = z.fillna(0)
+#	for i in range(q_low,q_up+1):
+#		z.ix[0,i] = math.exp(-1*alpha*kappa*i*i)
+#	tempnp = temp.as_matrix()
+#	znp = z.as_matrix().ravel()
+#	temp2 = numpy.dot(scipy.linalg.expm(tempnp),znp)
+#	return temp2.ix[q]
 
 
 
@@ -80,10 +92,12 @@ def u(t,q):
     return lambda_neg*math.exp(-1*kappa*buy_optimal_depth(t,q,kappa)) - lambda_pos*math.exp(-1*kappa*sell_optimal_depth(t,q,kappa))
 
 def get_sell_fill_rate(t,q):
-    return lambda_pos*math.exp(-1*kappa*sell1_optimal_depth(t,q,kappa))
+    return lambda_pos*math.exp(-1*kappa*sell_optimal_depth(t,q,kappa))
 
 def get_buy_fill_rate(t,q):
-    return lambda_neg*math.exp(-1*kappa*buy1_optimal_depth(t,q,kappa))
+    return lambda_neg*math.exp(-1*kappa*buy_optimal_depth(t,q,kappa))
+
+
 
 # Graph 10.1
 
@@ -95,7 +109,8 @@ graph101 = pd.DataFrame(index=rngt, columns=rng)
 
 for x in rngt:
     for y in rng:
-	graph101[x,y] = sell_optimal_depth(x,y,kappa)
+	graph101[x,y] = df
+     #graph101[x,y] = sell_optimal_depth(x,y,kappa)
 
 graph101.plot()
 
