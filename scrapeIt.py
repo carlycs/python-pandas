6fb2495ac64b45b60041edd2d6a92e0c8098d138
office206 <hp@lotustech.io>
2016-11-26 19:44:48 -0500

    first test

--- scrapeIt.py
+++ scrapeIt.py
@@ -1,4 +1,14 @@
-import webbrowser
+import webbrowser, sys
+import requests
 #for Cygwin add 
 #export BRWOSER=cygwin
 webbrowser.open('http://inventwithpython.com/')
+
+if len(sys.argv) > 1 :
+	#get address from command line
+	address=' '.join(sys.argv[1:])
+	
+
+
+res = requests.get('https://automatetheboringstuff.com/files/rj.txt')
+type(res)
