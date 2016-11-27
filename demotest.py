6979ab0a9e9ca408ef1f4a854563950ee9e1d240
office206 <hp@lotustech.io>
2016-11-27 15:15:24 -0500

    push updates- reademails IMAP

--- demotest.py
+++ demotest.py
@@ -18,6 +18,7 @@ def main():
 	print('Hi '+ myName + ', Did you know your name is %d letters long?' % len(myName))
 	sendEmailSignUp(myName)
 	#sendsms()
+	readEmail()
 
 
 
@@ -54,8 +55,57 @@ def sendsms():
 	to="+18144043047", 
 	from_="+17175430035", 
 	body="Hi, this is a reminder", 
-	media_url="", 
+	#media_url="", 
 ) 
+
+def readEmail():
+	import imaplib
+	mail = imaplib.IMAP4_SSL('imap.gmail.com')
+	mail.login('clientusername@gmail.com', 'clientpassword')
+	mail.list()
+	# Out: list of "folders" aka labels in gmail.
+	mail.select("inbox") # connect to inbox.
+	#Getting all mail and fetching the latest
+	result, data = mail.search(None, "ALL") 
+	ids = data[0] # data is a list.
+	id_list = ids.split() # ids is a space separated string
+	latest_email_id = id_list[-1] # get the latest
+	 
+	result, data = mail.fetch(latest_email_id, "(RFC822)") # fetch the email body (RFC822) for the given ID
+	 
+	raw_email = data[0][1] # here's the body, which is raw text of the whole email
+	# including headers and alternate payloads
+	#using uid
+	result, data = mail.uid('search', None, "ALL") # search and return uids instead
+	latest_email_uid = data[0].split()[-1]
+	result, data = mail.uid('fetch', latest_email_uid, '(RFC822)')
+	raw_email = data[0][1]
+
+	import email
+	email_message = email.message_from_string(raw_email)
+	 
+	print email_message['To']
+	 
+	print email.utils.parseaddr(email_message['From']) # for parsing "Yuji Tomita" <yuji@grovemade.com>
+	 
+	print email_message.items() # print all headers
+	 
+	# note that if you want to get text content (body) and the email contains
+	# multiple payloads (plaintext/ html), you must parse each message separately.
+	# use something like the following: (taken from a stackoverflow post)
+	def get_first_text_block(self, email_message_instance):
+	    maintype = email_message_instance.get_content_maintype()
+	    if maintype == 'multipart':
+	        for part in email_message_instance.get_payload():
+	            if part.get_content_maintype() == 'text':
+	                return part.get_payload()
+	    elif maintype == 'text':
+	        return email_message_instance.get_payload()
+
+	#IMAP protocol documentation https://tools.ietf.org/html/rfc3501
+	#https://yuji.wordpress.com/2011/06/22/python-imaplib-imap-example-with-gmail/        
+	mail.close()
+	mail.logout()
 
 if __name__ == '__main__':
 	main()
